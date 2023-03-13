71a0481c050f3e4bc722a071ed63a4d47d100e8e
v1-77a1n <v1_77a1n@icloud.com>
2023-03-13 13:20:47 -0400

    Added a POST routing to handle user account sign ups

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,11 @@ const controller = require('../controllers/userController');
 const router = express.Router();
 
 
-
+//Routing for signup page
 router.get('/new', controller.new);
+
+//Routing for signup POST
+router.post('/new', controller.newUser);
 
 //Routing for login
 router.get('/login', controller.login);
