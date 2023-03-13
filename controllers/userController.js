302b15e1c5679d82b0f9250db94c221c7abdda95
v1-77a1n <v1_77a1n@icloud.com>
2023-03-13 13:22:36 -0400

    Added controller for signup POST routing.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,34 @@
+const model = require('../models/user');
+const bcrypt = require('bcrypt');
 
+//renders the signup page
 exports.new = (req, res)=>{
     return res.render('./user/new');
 };
 
+//handles signup POST request
+exports.newUser = (req, res, next) => {
+    let user = new model(req.body);
+    user.save()
+    .then(() => res.redirect('/users/login'))
+    .catch(err => {
+        if(err.name === 'ValidationError') {
+            err.status = 400;
+            err.message = "Bad Database Request";
+        }
+
+        if(err.name === 'MongoError') {
+            err.status = 400;
+            err.message = "Bad Database Request";
+        }
+
+        next(err);
+
+    });
+};
+
+//renders the login page
 exports.login = (req, res) => {
     res.render('./user/login');
-}
+};
 
