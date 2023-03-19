8087fbe0c34ad97267e47d77bddf00c2bb2a08ce
v1-77a1n <v1_77a1n@icloud.com>
2023-03-19 09:46:48 -0400

    ST-8/9; additional Mongo Schema to handle the temporary storing of ids and tokens to track and validate guest password requests.

---
+++ models/resetToken.js
@@ -0,0 +1,12 @@
+//MongoDB collection with TTL index
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt'); //compare URL params to stored hash
+
+const resetTokenSchema = new Schema({
+    createdAt: {type: Date, required: [true, 'cannot be empty']},
+    token: {type: String, required: [true, 'cannot be empty']},
+    hashed_id: {type: String, required: [true, 'cannot be empty']}
+});
+
+modules.exports = mongoose.model('RToken', 'resetTokenSchema');
