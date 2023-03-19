c7bca4052a5afb973ff4231f03d2e92482aa2e12
v1-77a1n <v1_77a1n@icloud.com>
2023-03-19 10:46:33 -0400

    Added method to compare stored tokens

--- models/resetToken.js
+++ models/resetToken.js
@@ -6,7 +6,13 @@ const bcrypt = require('bcrypt'); //compare URL params to stored hash
 const resetTokenSchema = new Schema({
     createdAt: {type: Date, required: [true, 'cannot be empty']},
     token: {type: String, required: [true, 'cannot be empty']},
-    hashed_id: {type: String, required: [true, 'cannot be empty']}
+    email: {type: String, required: [true, 'cannot be empty']}
 });
+
+//comparing token in URL to stored token
+resetTokenSchema.methods.compareTokens = function(urlToken) {
+    let rToken = this;
+    return bcrypt.compare(rToken.token, urlToken);
+};
 
 module.exports = mongoose.model('RToken', resetTokenSchema);
