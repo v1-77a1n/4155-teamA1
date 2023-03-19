976e189953e31ca9cbbf95bf7ebef4c13546510c
v1-77a1n <v1_77a1n@icloud.com>
2023-03-19 09:50:06 -0400

    ST8/9/18; Added auth middleware to pre-existing routes to enforce what users or guests can access. Also added routing for guest password requests

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const controller = require('../controllers/userController');
+const {isGuest, isLoggedIn} = require('../middlewares/auth');
 const router = express.Router();
 
 
 //Routing for signup page
-router.get('/new', controller.new);
+router.get('/new', isGuest, controller.new);
 
 //Routing for signup POST
-router.post('/new', controller.newUser);
+router.post('/new', isGuest, controller.newUser);
 
 //Routing for login
-router.get('/login', controller.login);
+router.get('/login', isGuest, controller.login);
+
+//Routing for requesting password reset as Guest
+router.get('/req-pass-change', isGuest, controller.forgotPassword);
+
+//Routing for sending password reset link
+router.post('/req-pass-change', isGuest, controller.sendPasswordRest);
 
 //Routing for change account settings
-router.get('/change-email', controller.changeEmail);
-router.get('/change-password', controller.changePassword);
+router.get('/change-email', isLoggedIn, controller.changeEmail);
+router.get('/change-password', isLoggedIn, controller.changePassword);
 
 module.exports = router;
