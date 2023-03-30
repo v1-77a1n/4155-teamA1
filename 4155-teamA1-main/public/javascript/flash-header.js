c6d5984b331e7fc4ccada078eaadb9e0311f3331
Clarke Le <89429005+v1-77a1n@users.noreply.github.com>
2023-03-29 20:24:03 -0400

    Deleting Duplicate Directory

--- 4155-teamA1-main/public/javascript/flash-header.js
+++
@@ -1,5 +0,0 @@
-setTimeout(()=> {
-    const errorHeader = document.getElementById("errorHeader");
-
-    errorHeader.style.display = 'none';
-}, 2000);
--- 4155-teamA1-main/public/javascript/googleSignIn.js
+++
@@ -1,4 +0,0 @@
-function onSignIn(googleUser) {
-    var id_token = googleUser.getAuthResponse().id_token;
-    // Send the id_token to your server for verification
-}
