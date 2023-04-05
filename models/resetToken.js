92c47fa4c3b399a45d75b99eb3d8f116d2293cad
Rashaad Jones <rashaadjones367@gmail.com>
2023-04-05 15:28:06 -0400

    Google button discard

--- models/resetToken.js
+++ models/resetToken.js
@@ -4,27 +4,27 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt'); //compare URL params to stored hash
 
 const resetTokenSchema = new Schema({
-    createdAt: {type: Date, required: [true, 'cannot be empty']},
-    token: {type: String, required: [true, 'cannot be empty']},
-    email: {type: String, required: [true, 'cannot be empty']}
+    createdAt: { type: Date, required: [true, 'cannot be empty'] },
+    token: { type: String, required: [true, 'cannot be empty'] },
+    email: { type: String, required: [true, 'cannot be empty'] }
 });
 
-resetTokenSchema.pre('save', function(next) {
+resetTokenSchema.pre('save', function (next) {
     let rToken = this;
-    if(!rToken.isModified('token')) {
+    if (!rToken.isModified('token')) {
         return next();
     } else {
         bcrypt.hash(rToken.token, 10)
-        .then((hash) => {
-            rToken.token = hash;
-            next();
-        })
-        .catch(err=>next(err));
+            .then((hash) => {
+                rToken.token = hash;
+                next();
+            })
+            .catch(err => next(err));
     }
 });
 
 //comparing token in URL to stored token
-resetTokenSchema.methods.compareTokens = function(urlToken) {
+resetTokenSchema.methods.compareTokens = function (urlToken) {
     let rToken = this;
     return bcrypt.compare(urlToken, rToken.token);
 };
