63f9c14b69182aa8cbd6eb904812b923e251cb52
v1-77a1n <v1_77a1n@icloud.com>
2023-04-07 21:21:51 -0400

    ST-25: Fixed incorrect commit message / added a message schema aka new collection to database for messages.

--- models/message.js
+++ models/message.js
@@ -5,7 +5,7 @@ const messageSchema = new Schema({
     sender: {type: Schema.Types.ObjectId, required: [true]},
     recipient: {type: Schema.Types.ObjectId, required: [true]},
     subject: {type: String, required: [true, 'cannot be empty']},
-    msg: {type: String, required: [true, 'cannot be empty'], max: 10}
+    msg: {type: String, required: [true, 'cannot be empty'], min: 10}
 });
 
 module.exports = mongoose.model('Message', messageSchema);
