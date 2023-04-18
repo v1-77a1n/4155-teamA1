7b8ce0cf212bc3dd1bb5b4377d78a160acc7a244
v1-77a1n <jle17@uncc.edu>
2023-04-17 22:59:27 -0400

    ST-24; added forgotten ObjectId refs to message Schema

--- models/message.js
+++ models/message.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-    sender: {type: Schema.Types.ObjectId, required: [true]},
-    recipient: {type: Schema.Types.ObjectId, required: [true]},
+    sender: {type: Schema.Types.ObjectId, ref: 'User', required: [true]},
+    recipient: {type: Schema.Types.ObjectId, ref: 'User', required: [true]},
     subject: {type: String, required: [true, 'cannot be empty']},
     msg: {type: String, required: [true, 'cannot be empty'], min: 10}
 });
