005a021ec5da3c9bfad44389c20ce536fd064fe7
v1-77a1n <v1_77a1n@icloud.com>
2023-04-20 08:53:21 -0400

    ST-24 updated message model

--- models/message.js
+++ models/message.js
@@ -5,7 +5,16 @@ const messageSchema = new Schema({
     sender: {type: Schema.Types.ObjectId, ref: 'User', required: [true]},
     recipient: {type: Schema.Types.ObjectId, ref: 'User', required: [true]},
     subject: {type: String, required: [true, 'cannot be empty']},
-    msg: {type: String, required: [true, 'cannot be empty'], min: 10}
+    msg: {type: String, required: [true, 'cannot be empty'], min: 10},
+    createdAt: {type: String}
 });
+
+messageSchema.pre('save', function(next) {
+    let message = this;
+    let date = new Date();
+    let currDate = date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear();
+    message.createdAt = currDate;
+    next();
+})
 
 module.exports = mongoose.model('Message', messageSchema);
